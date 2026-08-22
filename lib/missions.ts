7006667b76614f8b46f45c7609fee549eb9d2068
OxOmisosiru <63462098+OxOmisosiru@users.noreply.github.com>
2026-08-22 11:51:24 +0900

    Update missions.ts

--- lib/missions.ts
+++ lib/missions.ts
@@ -91,7 +91,7 @@ export const missions: MissionDefinition[] = [
     index: 11,
     title: "Mission 11: 〇ッ〇ーしようぜ！",
     description: "クリア条件：以下の画像の指示を実際に実行すること\n\n",
-    image: "/missions/15_shiyouze.png",
+    image: "/missions/Mission11.png",
     answerType: "none",
   },
   {
@@ -104,7 +104,7 @@ export const missions: MissionDefinition[] = [
   {
     index: 13,
     title: "Mission 13: Betrayer",
-    description: "クリア条件：以下の30人に"直接"聞き込みをして、あるレースにおけるそれぞれのメンバーの順位を特定し受付で報告すること\n\nA.I.\nコーチョー\n直角\nsmi\nグマ\ntomato\nなぬ\nはてなぞ\nyahamasa\nりょう\nおぉ味噌汁\nRILD\nいっちー\nｎ型半導体\nリリバモリウム\nこがらし\nت74\n真\nしば\nふじみ\nどるみーる\nゐぬ娘。\nKy'3\nりんご\n逆凪\nリア\nきりけも\nながりゅう\n盈月\nShige\n\n1人だけ嘘をついている人物がいます。それ以外の人は全員本当のことを言っています。\n公演中のスタッフには聞き込みをしないでください。",
+    description: "クリア条件：以下の30人に 直接 聞き込みをして、あるレースにおけるそれぞれのメンバーの順位を特定し受付で報告すること\n\nA.I.\nコーチョー\n直角\nsmi\nグマ\ntomato\nなぬ\nはてなぞ\nyahamasa\nりょう\nおぉ味噌汁\nRILD\nいっちー\nｎ型半導体\nリリバモリウム\nこがらし\nت74\n真\nしば\nふじみ\nどるみーる\nゐぬ娘。\nKy'3\nりんご\n逆凪\nリア\nきりけも\nながりゅう\n盈月\nShige\n\n1人だけ嘘をついている人物がいます。それ以外の人は全員本当のことを言っています。\n公演中のスタッフには聞き込みをしないでください。",
     image: "",
     answerType: "none",
   },
